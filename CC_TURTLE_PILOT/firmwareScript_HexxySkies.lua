703d8f0c8bd682e51da9e0d0b47e4c01e2dcbe03
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 07:26:54 +0800

    Update firmwareScript_HexxySkies.lua

--- CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
+++ CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
@@ -7,12 +7,6 @@ local instance_configs = {}
 
 local drone = DroneBaseClassHexxySkies:subclass()
 
---[[
-function drone:getOffsetDefaultShipOrientation(default_ship_orientation)
-	return quaternion.fromRotation(default_ship_orientation:localPositiveY(), 0)*default_ship_orientation
-end
-]]--
-
 function drone:customFlightLoopBehavior(customFlightVariables)
 	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(0,0,1))*self.target_rotation
 	--self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(1,0,0))*self.target_rotation
