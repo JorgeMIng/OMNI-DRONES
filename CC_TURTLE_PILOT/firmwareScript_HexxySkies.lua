ccde32a26331699ea3d55e527369b6a4ac0bb3b6
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 07:29:28 +0800

    Update firmwareScript_HexxySkies.lua

--- CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
+++ CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
@@ -5,7 +5,7 @@ local quaternion = require "lib.quaternions"
 
 local instance_configs = {}
 
-local drone = DroneBaseClassHexxySkies:subclass()
+local drone = DroneBaseClassHexxySkies(instance_configs)
 
 function drone:customFlightLoopBehavior(customFlightVariables)
 	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(0,0,1))*self.target_rotation
@@ -13,6 +13,4 @@ function drone:customFlightLoopBehavior(customFlightVariables)
 	self.target_global_position = vector.new(X,Y,Z)
 end
 
-local customDrone = drone(instance_configs)
-
-customDrone:run()
+drone:run()
