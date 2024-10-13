694f43e730f3a6129bf51f06fcec1dfd49c4b30b
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 07:26:00 +0800

    Update firmwareScript_HexxySkies.lua

--- CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
+++ CC_TURTLE_PILOT/firmwareScript_HexxySkies.lua
@@ -1,22 +1,22 @@
 --For HexxySkies and HexTweaks 4.0.0+
 local DroneBaseClassHexxySkies = require "lib.tilt_ships.DroneBaseClassHexxySkies"
 
-local Path = require "lib.paths.Path"
-local path_utilities = require "lib.path_utilities"
 local quaternion = require "lib.quaternions"
-local JSON = require "lib.JSON"
 
 local instance_configs = {}
 
 local drone = DroneBaseClassHexxySkies:subclass()
+
 --[[
 function drone:getOffsetDefaultShipOrientation(default_ship_orientation)
 	return quaternion.fromRotation(default_ship_orientation:localPositiveY(), 0)*default_ship_orientation
 end
 ]]--
+
 function drone:customFlightLoopBehavior(customFlightVariables)
-	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveY(),vector.new(0,1,0))*self.target_rotation
-	self.target_global_position = vector.new(-5,10,-8)
+	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(0,0,1))*self.target_rotation
+	--self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(1,0,0))*self.target_rotation
+	self.target_global_position = vector.new(X,Y,Z)
 end
 
 local customDrone = drone(instance_configs)
