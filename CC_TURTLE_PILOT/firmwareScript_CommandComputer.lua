a3ca623f4e0a6c5fe74c653a6d0982e114287e20
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 07:34:05 +0800

    Update firmwareScript_CommandComputer.lua

--- CC_TURTLE_PILOT/firmwareScript_CommandComputer.lua
+++ CC_TURTLE_PILOT/firmwareScript_CommandComputer.lua
@@ -10,7 +10,7 @@ local drone = DroneBaseClassCommandComputer(instance_configs)
 function drone:customFlightLoopBehavior(customFlightVariables)
 	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(0,0,1))*self.target_rotation
 	--self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(1,0,0))*self.target_rotation
-	self.target_global_position = vector.new(X,Y,Z)
+	self.target_global_position = vector.new(X,Y,Z) --replace XYZ with world coordinates
 end
 
 drone:run()
