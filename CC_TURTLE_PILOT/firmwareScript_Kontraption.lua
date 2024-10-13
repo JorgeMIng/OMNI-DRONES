a5afe7630c9053860d22edee7a338b826bee1e4c
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 07:30:27 +0800

    Update firmwareScript_Kontraption.lua

--- CC_TURTLE_PILOT/firmwareScript_Kontraption.lua
+++ CC_TURTLE_PILOT/firmwareScript_Kontraption.lua
@@ -18,20 +18,12 @@ local instance_configs = {
     	},
 }
 
-local drone = DroneBaseClassKontraption:subclass()
+local drone = DroneBaseClassKontraption(instance_configs)
 
---[[
-function drone:getOffsetDefaultShipOrientation(default_ship_orientation)
-	return quaternion.fromRotation(default_ship_orientation:localPositiveY(), -90)*default_ship_orientation
-end
-]]--
-	
 function drone:customFlightLoopBehavior(customFlightVariables)
 	self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(0,0,1))*self.target_rotation
 	--self.target_rotation = quaternion.fromToRotation(self.target_rotation:localPositiveX(),vector.new(1,0,0))*self.target_rotation
 	self.target_global_position = vector.new(X,Y,Z)
 end
 
-local customDrone = drone(instance_configs)
-
-customDrone:run()
+drone:run()
