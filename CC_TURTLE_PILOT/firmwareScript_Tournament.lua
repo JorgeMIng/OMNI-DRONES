baad13291aa688e3bb7af30d4edb5fc3a1008fb9
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-10-14 08:06:25 +0800

    Update firmwareScript_Tournament.lua

--- CC_TURTLE_PILOT/firmwareScript_Tournament.lua
+++ CC_TURTLE_PILOT/firmwareScript_Tournament.lua
@@ -217,7 +217,7 @@ end
 
 -- Watch this tutorial video to learn how to use this function: https://youtu.be/07Czgxqp0dk?si=gltpueMIgFjHpqJZ&t=269 (skip to 4:29)
 function drone:getOffsetDefaultShipOrientation(default_ship_orientation)
-	return quaternion.fromRotation(default_ship_orientation:localPositiveY(), -90)*default_ship_orientation
+	return quaternion.fromRotation(default_ship_orientation:localPositiveY(), -90)*default_ship_orientation -- Rotates the default orientation so that the nose of the ship is aligned with it's local +X axis
 end
 
 -- Watch this tutorial video to learn how to use this function: https://youtu.be/07Czgxqp0dk?si=gltpueMIgFjHpqJZ&t=269 (5:25 to 10:30)
